5508045d3f8ef4f3ae9edf2dbfd2af2c58afaea9
XiangLuc <xiangluc01@gmail.com>
2024-11-18 15:58:19 +0100

    class dto user

---
+++ backend/shared/dto/schemas/user_dto.py
@@ -0,0 +1,11 @@
+user_dto = {
+    "type": "object",
+    "properties": {
+        "first_name": {"type": "string"},
+        "last_name": {"type": "string"},
+        "email": {"type": "string", "format": "email"},
+        "role": {"type": "string", "enum": ["admin", "user"]},
+    },
+    "required": ["first_name", "last_name", "email", "role"],
+    "additionalProperties": False
+}
