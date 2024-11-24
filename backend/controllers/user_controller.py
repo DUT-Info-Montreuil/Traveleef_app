bc317edfb4e8a04bdb7c6c3aba5be77056863c30
XiangLuc <xiangluc01@gmail.com>
2024-11-24 18:58:24 +0100

    mise en place d'un module pour mieux regrouper service, schema et mapper.

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,13 +1,10 @@
 from flask import Blueprint, request, jsonify
 from flask_expects_json import expects_json
-from infra.db.postgresql_driver import PostgresqlDriver
-from services.user_service import UserService
-from shared.dto.schemas.user_dto import user_dto as user_dto_schema
-from domain.repositories.user_repository import UserRepository
+from shared.modules.user_module import user_module
 
-database = PostgresqlDriver()
-user_repository = UserRepository(database)
-user_service = UserService(user_repository)
+user_dto_schema = user_module['dto_schema']
+service = user_module['service']
+mapper = user_module['dto_mapper']
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -17,8 +14,8 @@ user_bp = Blueprint('user_bp', __name__)
 def create_user():
     dto = request.json
     try:
-        created_user = user_service.create_user(dto)
-        return jsonify({"message": "User created successfully!", "user": created_user.__repr__()}), 200
+        created_user = service.create_user(dto)
+        return jsonify({"message": "User created successfully!", "user": mapper.to_external(created_user)}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -33,7 +30,7 @@ def login():
         return jsonify({"error": "Email and password are required"}), 400
 
     try:
-        login_response = user_service.login_user(email, password)
+        login_response = service.login_user(email, password)
         return jsonify({"access_token": login_response['access_token'], "role": login_response['role']}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 401
