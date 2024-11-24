d8abb130e970cdb4f587926d6510c6ef98eff297
XiangLuc <xiangluc01@gmail.com>
2024-11-24 17:34:15 +0100

    ajout d'un champs password

--- backend/shared/dto/schemas/user_dto.py
+++ backend/shared/dto/schemas/user_dto.py
@@ -5,6 +5,7 @@ user_dto = {
         "last_name": {"type": "string"},
         "email": {"type": "string", "format": "email"},
         "role": {"type": "string", "enum": ["admin", "user"]},
+        "password": {"type": "string"}
     },
     "required": ["first_name", "last_name", "email", "role"],
     "additionalProperties": False
