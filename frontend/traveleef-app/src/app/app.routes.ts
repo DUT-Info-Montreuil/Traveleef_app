109a6763b8fa6e10f3637e0678d7d30f0e8bbcf9
XiangLuc <xiangluc01@gmail.com>
2024-12-12 18:37:38 +0100

    Ajout d'une route par défault qui redirige sur la page d'accueil.

--- frontend/traveleef-app/src/app/app.routes.ts
+++ frontend/traveleef-app/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from "./features/accueil/page/accueil/accueil.component";
-import {TravelListeComponent} from "./features/travel/page/travel-liste/travel-liste.component";
 
 export const routes: Routes = [
   {path: "accueil", component: AccueilComponent},
-  {path: "resultats", component: TravelListeComponent},
+  { path: "", redirectTo: "accueil", pathMatch: "full" },
 ];
