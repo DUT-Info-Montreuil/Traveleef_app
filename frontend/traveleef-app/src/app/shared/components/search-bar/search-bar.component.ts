5c8644532d90516faf91b4aca20646b3a34bcd6b
XiangLuc <xiangluc01@gmail.com>
2024-12-12 18:33:45 +0100

    Ajout du providers "httpClient" afin de pouvoir faire des requetes Http depuis le frontend vers le backend ou tout autre type de service externe.

--- frontend/traveleef-app/src/app/shared/components/search-bar/search-bar.component.ts
+++ frontend/traveleef-app/src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgForOf, NgIf} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search-bar',
@@ -8,18 +9,31 @@ import {NgForOf, NgIf} from "@angular/common";
   imports: [
     FormsModule,
     NgForOf,
-    NgIf
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
 
+  searchForm: FormGroup;
+
   nombreVoyageurs = {
     adulte: 1,
     enfant: 0,
     bebe: 0
   };
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.searchForm = this.fb.group({
+      departureId: ['', Validators.required],
+      arrivalId: ['', Validators.required],
+      outboundDate: ['', Validators.required],
+      returnDate: [''],
+      tripType: ['',Validators.required],
+    });
+  }
 
   // Fonction permettant d'incrémenter ou décrémenter le nombre de voyageur.
   modifierNombreVoyageurs(type: 'adulte' | 'enfant' | 'bebe', valeur: number): void {
@@ -30,7 +44,24 @@ export class SearchBarComponent {
   }
 
   onSearch(): void {
-    console.log("data");
-  }
+    console.log("Entre");
+    if (this.searchForm.valid) {
+      const formData = this.searchForm.value;
 
+      const apiData = {
+        departure_id: formData.departureId,
+        arrival_id: formData.arrivalId,
+        outbound_date: formData.outboundDate,
+        return_date: formData.returnDate || null,
+        trip_type: formData.tripType,
+        adults: this.nombreVoyageurs.adulte,
+        children: this.nombreVoyageurs.enfant,
+        infants: this.nombreVoyageurs.bebe
+      };
+      console.log(apiData);
+      this.router.navigate(['/resultats'], { state: { data: apiData } });
+    } else {
+      console.log("Formulaire invalide");
+    }
+  }
 }
