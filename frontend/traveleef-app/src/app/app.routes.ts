229ddb861028bf40bdc85a29eaa1679bcce1c37e
Antoine23o0 <ngatchouantoin1@gmail.com>
2024-12-15 17:41:05 +0100

    page voyage detail

--- frontend/traveleef-app/src/app/app.routes.ts
+++ frontend/traveleef-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import {PageResultatRechercheComponent} from "./page-resultat-recherche/page-resultat-recherche.component";
+import { VoyageDetailComponent } from './voyage-detail/voyage-detail.component';
 
 
 
@@ -9,5 +10,6 @@ export const routes: Routes = [
     {path : 'inscription', component : InscriptionComponent},
     {path : 'connexion', component : ConnexionComponent},
     {path: 'results', component: PageResultatRechercheComponent},
+    { path: 'voyage-detail', component: VoyageDetailComponent },
     { path: '', redirectTo: '/connexion', pathMatch: 'full' }, // Redirection par défaut
 ];
